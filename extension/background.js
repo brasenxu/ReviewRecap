a7dadcec8746b968b20c0431cfcfc13898fbeb7b
Steven <wusteven815@gmail.com>
2023-02-11 23:19:26 -0500

    Refactor to clean

--- extension/background.js
+++ extension/background.js
@@ -1,17 +1,19 @@
 const checkIcon = () => {
+
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs.length != 0 && tabs[0].url) {
-            if (
-                tabs &&
-                tabs[0].url.includes("amazon.") &&
-                tabs[0].url.includes("/dp/")
-            ) {
-                chrome.action.setIcon({ path: "assets/recapOn.png" });
-            } else {
-                chrome.action.setIcon({ path: "assets/recapOff.png" });
-            }
+
+        if (tabs.length === 0 || !tabs[0].url) {
+            return;
         }
+
+        if (tabs && tabs[0].url.includes("amazon.") && tabs[0].url.includes("/dp/")) {
+            chrome.action.setIcon({ path: "assets/recapOn.png" });
+        } else {
+            chrome.action.setIcon({ path: "assets/recapOff.png" });
+        }
+
     });
+
 };
 
 chrome.tabs.onUpdated.addListener(checkIcon);
