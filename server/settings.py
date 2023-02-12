b49363ea91ca66092bbdcdd2127076df03f81a51
Steven <wusteven815@gmail.com>
2023-02-11 22:11:12 -0500

    Allow host

--- server/settings.py
+++ server/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.96.213"]
 
 
 # Application definition
