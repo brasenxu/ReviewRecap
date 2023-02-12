7cc0bf3b44628683dce754fd4b83db5ed1096d04
Steven <wusteven815@gmail.com>
2023-02-11 21:49:13 -0500

    Move to parent dir

--- ReviewRecap/settings.py
+++ ReviewRecap/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "server.ReviewRecap",
+    "ReviewRecap",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- ReviewRecap/urls.py
+++ ReviewRecap/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from server.ReviewRecap import views
-
+from ReviewRecap import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
