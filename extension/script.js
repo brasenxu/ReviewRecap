763ac3a9c645d3ed4240bff8b4c5b20c0ef5edd5
Steven <wusteven815@gmail.com>
2023-02-11 23:58:15 -0500

    Refactor to clean

--- extension/script.js
+++ extension/script.js
@@ -1,34 +1,21 @@
-(() => {
-    let isOn = false;
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        let url = tabs[0].url;
-        if (url.includes("amazon.") && url.includes("/dp/")) {
-            chrome.action.setIcon({ path: "assets/recapOn.png" });
-            document.getElementById("section").innerHTML =
-                "Click button to recap Amazon reviews.";
-            document.getElementById("recapButn").innerHTML =
-                '<a href="recap.html" class="btn btn-primary justify-content-center" role="button">RECAP</a>';
-            isOn = true;
-        } else {
-            chrome.action.setIcon({ path: "assets/recapOff.png" });
-            document.getElementById("section").innerHTML =
-                "Current tab is not able to be recapped.";
-            document.getElementById("recapButn").innerHTML =
-                '<a class="btn btn-primary justify-content-center disabled" role="button" aria-disabled="true"> RECAP </a>';
-            isOn = false;
-        }
-        const indexdp = url.indexOf("/dp/");
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 
-        url = url.substring(19, indexdp + 14);
-        const parts = url.split("/");
+    let url = tabs[0].url;
 
-        if (parts.length == 4) {
-            parts.splice(1, 1);
-        }
+    if (url.includes("amazon.") && url.includes("/dp/")) {
+        chrome.action.setIcon({ path: "assets/recapOn.png" });
+        document.getElementById("section").innerHTML =
+            "Click button to recap Amazon reviews.";
+        document.getElementById("recapButn").innerHTML =
+            '<a href="recap.html" class="btn btn-primary justify-content-center" role="button">RECAP</a>';
 
-        const newURL = parts.join("/");
-        const encodedData = btoa(newURL);
+    } else {
+        chrome.action.setIcon({ path: "assets/recapOff.png" });
+        document.getElementById("section").innerHTML =
+            "Current tab is not able to be recapped.";
+        document.getElementById("recapButn").innerHTML =
+            '<a class="btn btn-primary justify-content-center disabled" role="button" aria-disabled="true">RECAP</a>';
+    }
 
-        console.log(encodedData);
-    });
-})();
+});
+
